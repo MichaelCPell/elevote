660c86db5d221773f99a977a3dd7e2d2008f2b92
unknown <Michael@.(none)>
2012-01-09 23:19:50 -0500

    first heroku push

--- db/migrate/20111127035427_create_districts.rb
+++ db/migrate/20111127035427_create_districts.rb
@@ -3,8 +3,8 @@ class CreateDistricts < ActiveRecord::Migration
     create_table :districts do |t|
       t.string :category
       t.string :number
-      t.string :gpe
       t.integer :office_id
+      t.string :gpe
 
       t.timestamps
     end
