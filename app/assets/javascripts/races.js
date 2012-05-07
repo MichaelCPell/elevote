83d2bfa2d4f577af54b0b3ef0bfe3cdd124a19ff
Michael Pell <MichaelCPell@gmail.com>
2012-05-07 16:10:05 -0400

    Created and Polished Candidate Display Modals

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -1,26 +1,22 @@
-
-
 $(document).ready( function(){
 
-    $('.btn').click(
-        function(){
-            $(this).toggleClass('btn-danger').toggleClass('btn-primary');
-
-//    Ran into strange errors when trying to do this with a .toggle() function
-            if($(this).text() == "Add to Booth")
-            {
-                $(this).text("Remove from Booth");
-            }
-            else
-            {
-                $(this).text("Add to Booth");
-             }
-        }
-    )
-
-
-
+  $('.btn').click( function(){
+    $(this).toggleClass('btn-danger').toggleClass('btn-primary');
+    //    Ran into strange errors when trying to do this with a .toggle() function
+    if($(this).text() == "Add to Booth"){
+      $(this).text("Remove from Booth");
+    }else{
+    $(this).text("Add to Booth");
+    }
+  })
 });
 
+$(document).ready(function(){
+
+   $(".modal_trigger").click(function(){
+    $('#myModal').modal();
+   });
+
+});
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-
-
-
-
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     #id = new_object.object_id
@@ -22,12 +18,5 @@ module ApplicationHelper
     </div>
     "
   end
-
-
-
-
-
-
-
 
 end
