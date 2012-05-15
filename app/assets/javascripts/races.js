3590a8d46c209f74e6bfc2aa44c8c2a58f88c168
Michael Pell <MichaelCPell@gmail.com>
2012-05-15 18:08:32 -0400

    Started Fixing Users#show

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -3,22 +3,16 @@ $(document).ready( function(){
   $('.booth-button').click( function(){
     $(this).toggleClass('btn-danger').toggleClass('btn-primary');
     //    Ran into strange errors when trying to do this with a .toggle() function
+
     if($(this).text() == "Add to Booth"){
       $(this).text("Remove from Booth");
     }else{
     $(this).text("Add to Booth");
     }
 
-  })
+  });
 });
 
-$(document).ready(function(){
-
-   $(".modal_trigger").click(function(){
-    $('#myModal').modal();
-   });
-
-});
 
 
 
