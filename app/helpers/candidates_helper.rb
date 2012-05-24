c4e755081a2d2ea998c22ff32f16ff043e7587de
Michael Pell <MichaelCPell@gmail.com>
2012-05-24 16:02:39 -0400

    Turned Candidate Column Names into Buttons

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -24,4 +24,22 @@ module CandidatesHelper
     end
   end
 
+   def is_in_booth(id)
+
+    if session[:candidate_ids].index(id.to_s)
+      link_to "Remove from Booth",
+            new_endorsement_path(:candidate_id => id),
+            :remote => true,
+            :class => "btn btn-danger booth-button",
+            :style => "width: 200px; font-size: 18px;"
+    else
+      link_to "Add to Booth",
+            new_endorsement_path(:candidate_id => id),
+            :remote => true,
+            :class => "btn btn-primary booth-button",
+            :style => "width: 200px; font-size: 18px;"
+    end
+
+  end
+
 end
--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,19 +1,3 @@
 module RacesHelper
-  def is_in_booth(id)
 
-    if session[:candidate_ids].index(id.to_s)
-      link_to "Remove from Booth",
-            new_endorsement_path(:candidate_id => id),
-            :remote => true,
-            :class => "btn btn-danger booth-button",
-            :style => "width: 200px; font-size: 18px;"
-    else
-      link_to "Add to Booth",
-            new_endorsement_path(:candidate_id => id),
-            :remote => true,
-            :class => "btn btn-primary booth-button",
-            :style => "width: 200px; font-size: 18px;"
-    end
-
-  end
 end
